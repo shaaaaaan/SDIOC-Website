106c336edb8e3287013cbbad21253b4a166f83ba
shaaaaaan <shan.sunny@hotmail.com>
2026-07-01 20:18:47 +1200

    Add map icon and generally bootstrap icon CDN

--- src/app/common/footer/footer.ts
+++ src/app/common/footer/footer.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './footer.html'
 })
-export class Footer {}
+export class Footer {
+  openGoogleMaps() {
+    window.open('https://www.google.com/maps/place/St.+Dionysius+Indian+Orthodox+Church+(SDIOC)/data=!4m2!3m1!1s0x0:0x7222ea64f974a95b')?.focus();
+  }
+}
